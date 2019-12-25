5f0a4cfa476c351058c869fa2341c053a548dc6b
rakaiqbalsy <rakaiqbalsy@gmail.com>
2019-12-26 04:23:43 +0700

    Game Percobaan

---
+++ Assets/main_menu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main_menu : MonoBehaviour {
+
+	public void GoToMainMenu(){
+		Application.LoadLevel("main_menu");
+	}
+
+	public void GoToARCamera(){
+		Application.LoadLevel("ARCamera");
+	}
+
+    public void GoToPreGame()
+    {
+        Application.LoadLevel("preGame");
+    }
+
+    public void GoToGamePlay()
+    {
+        Application.LoadLevel("GamePlay");
+    }
+    public void GoToInfo()
+    {
+        Application.LoadLevel("Info");
+    }
+
+     public void GoToAboutUs() {
+         Application.LoadLevel("About");
+    }
+
+	public void ExitApplication(){
+		Application.Quit ();
+	}
+}
---
+++ Assets/splash.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class splash : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() {
+		StartCoroutine(Example());
+	}
+
+	IEnumerator Example() {
+		yield return new WaitForSeconds(2);
+		Application.LoadLevel ("loading");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
